b57761abf3c74275be16cba529aadcade43fee32
Keshav Gupta <keshavgupta150107@gmail.com>
2026-07-08 17:24:33 +0530

    Added solution - LeetHub

---
+++ 23447-java-loops-i-Hackerrank/23447-java-loops-i.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i=1;i<11;i++){
+            int p= N*i;
+            System.out.println(N + " x " + i+ " = " + p) ;
+        }
+        bufferedReader.close();
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
